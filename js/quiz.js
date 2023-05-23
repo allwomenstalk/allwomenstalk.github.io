43db996a165f9c33c1323c919f46089c4bc986a2
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-05-23 16:27:32 -0400

    new template for quiz

--- js/quiz.js
+++ js/quiz.js
@@ -21,17 +21,16 @@ function generateHTML(data) {
     data.forEach(item => {
         // Generate new HTML content
         const newHtml = `
-        <div class="bg-white rounded-md ">
-            <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
-                <div class="mx-auto max-w-lg text-center">
-                    <p class="text-base font-semibold leading-7 text-pink-600">Quiz Time</p>
-                    <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">${item.title}</h2>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="https://quiz.allwomenstalk.com/${item._id}" class="_quizwidget rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Play now</a>
-                    </div>
-                </div>
+        <div class="bg-indigo-100">
+            <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Engage in a quiz on<br>{{item.title}}</h2>
+            <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+                <a href="https://quiz.allwomenstalk.com/{{item._id}}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Start Quiz</a>
+                <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
             </div>
-        </div>`;
+            </div>
+        </div>      
+        `;
 
         // Append the new HTML content to the specific div
         quizzesDiv.innerHTML += newHtml;
