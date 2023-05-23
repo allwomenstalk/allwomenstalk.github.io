fb3678c9008458a4c2a3cfea3a4a6d9b7160f99a
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-05-23 17:07:26 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -22,10 +22,10 @@ function generateHTML(data) {
         // Generate new HTML content
         const newHtml = `
         <div class="bg-white">
-            <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Engage in a quiz on<br>${item.title}</h2>
+            <div class="mx-auto max-w-7xl px-6 py-12 sm:py-12 lg:flex lg:items-center lg:justify-between lg:px-8">
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Engage in a quiz.<br>${item.title}</h2>
             <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-                <a href="https://quiz.allwomenstalk.com/${item._id}" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Start Quiz</a>
+                <a href="https://quiz.allwomenstalk.com/${item._id}" class="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Start Quiz</a>
                 <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore more <span aria-hidden="true">→</span></a>
             </div>
             </div>
