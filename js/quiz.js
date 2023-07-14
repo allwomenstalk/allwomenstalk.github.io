799e1046dd748f07b5b1127f8294e3f255581c59
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-07-14 11:20:01 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -23,7 +23,7 @@ function generateHTML(data) {
         const newHtml = `
         <div class="bg-white rounded-md my-2">
             <div class="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Engage in a quiz.<br>${item.title}</h2>
+            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"><span class="text-gray-400"> Quiz:</span><br>${item.title}</h2>
             <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
                 <a href="https://quiz.allwomenstalk.com/${item._id}" class="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Start Quiz</a>
                 <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore more <span aria-hidden="true">→</span></a>
