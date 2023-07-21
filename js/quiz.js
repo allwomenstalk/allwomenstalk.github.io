6edb1304d702460faf83f670f2d7148fe8a03bc3
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-07-21 11:04:29 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -1,51 +1,59 @@
 // Function to fetch data from MongoDB
 function fetchData() {
-    fetch('https://us-east-1.aws.data.mongodb-api.com/app/azoio-evvkb/endpoint/quizzes', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }).then(response => response.json())
-      .then(data => generateHTML(data))
-      .catch((error) => {
-        console.log('Error:', error);
+  fetch(
+    "https://us-east-1.aws.data.mongodb-api.com/app/azoio-evvkb/endpoint/quizzes",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => generateHTML(data))
+    .catch((error) => {
+      console.log("Error:", error);
     });
 }
 
 // Function to generate HTML
 function generateHTML(data) {
-    const quizzesDiv = document.getElementById('quizzes'); // Get reference to the div
+  const quizzesDiv = document.getElementById("quizzes"); // Get reference to the div
 
-    data = [data]
-    console.log(data);
-    data.forEach(item => {
-        // Generate new HTML content
-        const newHtml = `
-        <div class="bg-white rounded-md my-2">
-            <div class="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
-            <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"><span class="text-gray-400"> Quiz:</span><br>${item.title}</h2>
-            <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-                <a href="https://quiz.allwomenstalk.com/${item._id}" class="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Start Quiz</a>
-                <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore more <span aria-hidden="true">→</span></a>
-            </div>
-            </div>
-        </div>      
-        `;
+  data = [data];
+  console.log("path", window.location.pathname);
+  wrapperclass = window.location.pathname == "/" ? "" : " mx-auto max-w-md ";
+  data.forEach((item) => {
+    // Generate new HTML content
+    const newHtml = `
+            <div class="rounded-md my-2 ${wrapperclass}">
+                <div class="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">${item.title}</h2>
+                <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+                    <a href="https://quiz.allwomenstalk.com/${item._id}" class="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Start Quiz</a>
+                    <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore more <span aria-hidden="true">→</span></a>
+                </div>
+                </div>
+            </div>      
+            `;
 
-        // Append the new HTML content to the specific div
-        quizzesDiv.innerHTML += newHtml;
-    });
+    // Append the new HTML content to the specific div
+    quizzesDiv.innerHTML += newHtml;
+  });
 }
 
 // Intersection Observer
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            console.log('Intersecting');
-            fetchData();
-            observer.disconnect(); // Stop observing after first intersection
-        }
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("Intersecting");
+        fetchData();
+        observer.disconnect(); // Stop observing after first intersection
+      }
     });
-}, {rootMargin: '0px 0px -10px 0px'}); // Adjust rootMargin as needed to control when the fetch happens
+  },
+  { rootMargin: "0px 0px -10px 0px" }
+); // Adjust rootMargin as needed to control when the fetch happens
 
-observer.observe(document.getElementById('quizzes')); // Observe the #quizzes div
+observer.observe(document.getElementById("quizzes")); // Observe the #quizzes div
