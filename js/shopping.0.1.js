60dce0bb1a0dfd968503dc5d395d4b9ab1c670b9
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-07-27 11:15:13 -0400

    update

--- js/shopping.0.1.js
+++ js/shopping.0.1.js
@@ -40,10 +40,6 @@ async function fetchAndDisplayProducts() {
         <div class="mx-auto max-w-xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-3xl lg:px-8">
           <div class="md:flex md:items-center md:justify-between">
             <h2 class="text-2xl font-bold tracking-tight text-gray-900">SHOP NOW<br> Trending Products ⚡️</h2>
-            <a href="#" class="hidden text-sm font-medium text-indigo-600 hover:text-indigo-500 md:block">
-              Shop the collection
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
           </div>
       
           <div class="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
@@ -51,10 +47,6 @@ async function fetchAndDisplayProducts() {
           </div>
       
           <div class="mt-8 text-sm md:hidden">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-              Shop the collection
-              <span aria-hidden="true"> &rarr;</span>
-            </a>
           </div>
         </div>
       </div>
