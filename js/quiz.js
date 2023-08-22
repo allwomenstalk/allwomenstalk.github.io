c4466f326630efc84f2d740476b45c55c98895d5
yapapahub <40893287+yapapahub@users.noreply.github.com>
2023-08-22 17:22:30 -0400

    update

--- js/quiz.js
+++ js/quiz.js
@@ -27,23 +27,27 @@ function generateHTML(data) {
 
   data = [data];
   console.log("path", window.location.pathname);
-  wrapperclass = window.location.pathname == "/" ? "" : " mx-auto max-w-md ";
+  wrapperclass = window.location.pathname == "/" ? "" : " mx-auto max-w-2xl px-8";
   data.forEach((item) => {
     // Generate new HTML content
     const newHtml = `
-            <div class="rounded-md my-2 ${wrapperclass}">
-                <div class="mx-auto max-w-7xl px-6 py-14 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">${item.title}</h2>
-                <div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+            <div class="my-2 ${wrapperclass}">
+                <div class="mx-auto max-w-7xl py-8 sm:py-16">
+                <div class="flex gap-x-6">
+                  <p class="text-sm font-semibold text-pink-600">Allwomenstalk Quiz</p>
+                  <a href="javaScript:fetchData()" class="text-sm font-semibold leading-6 text-gray-500">Change topic <span aria-hidden="true">↻</span></a>
+                </div>
+                <h2 class="my-10 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">${item.title}</h2>
+                <div class="flex items-center gap-x-6">
                     <a href="https://quiz.allwomenstalk.com/${item._id}/?postId=${postId}&host=${host}" class="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Start Quiz</a>
-                    <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore more <span aria-hidden="true">→</span></a>
+                    <a href="https://quiz.allwomenstalk.com/" class="text-sm font-semibold leading-6 text-gray-900">Explore More <span aria-hidden="true">→</span></a>
                 </div>
                 </div>
             </div>      
             `;
 
     // Append the new HTML content to the specific div
-    quizzesDiv.innerHTML += newHtml;
+    quizzesDiv.innerHTML = newHtml;
   });
 }
 
